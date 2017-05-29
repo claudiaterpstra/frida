c2ebf42008bc16123a3759f77e370379c1a8ef16
Hwei Tan <chienhwei.tan@gmail.com>
2017-05-29 15:33:30 +0100

    Created database and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529125350) do
+ActiveRecord::Schema.define(version: 20170529133234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "price"
+    t.integer  "rating"
+    t.string   "category"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_courses_on_user_id", using: :btree
+  end
+
+  create_table "participations", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_participations_on_course_id", using: :btree
+    t.index ["user_id"], name: "index_participations_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,4 +53,7 @@ ActiveRecord::Schema.define(version: 20170529125350) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "courses", "users"
+  add_foreign_key "participations", "courses"
+  add_foreign_key "participations", "users"
 end
