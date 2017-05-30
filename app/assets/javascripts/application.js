0d279ac4d2537df865d64ffac5384fddab1b4626
Rebekah Cheng <work@rebekahcheng.com>
2017-05-30 12:37:42 +0100

    added jquery-slick-rails gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,9 @@
 //= require jquery
 //= require jquery_ujs
+//= require jquery.slick
 //= require bootstrap-sprockets
 //= require_tree .
 
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
+
