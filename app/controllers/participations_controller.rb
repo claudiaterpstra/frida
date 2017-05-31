207667a4bb5daab6c865692d91c382a8327be78b
Hwei Tan <chienhwei.tan@gmail.com>
2017-05-31 11:10:00 +0100

    Participations...forget to commit beginning

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,7 +1,7 @@
 class ParticipationsController < ApplicationController
 
   def create
-    Participation.create!(user_id: current_user.id , course_id: @course)
-    redirect_to course_path(@course)
+    Participation.create!(user_id: current_user.id, course_id: @course)
+    redirect_to user_path(current_user)
   end
 end
