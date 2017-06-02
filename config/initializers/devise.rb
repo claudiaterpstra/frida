a05650218c0fe0ba4d10b573258b5c8cb3ecf647
Claudia Terpstra <claudia_terpstra@hotmail.com>
2017-06-02 14:10:03 +0100

    changed fb picture

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -255,7 +255,7 @@ Devise.setup do |config|
   config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"],
     scope: 'email',
     info_fields: 'email, first_name, last_name',
-    image_size: 'square',  # 50x50, guaranteed ratio
+    image_size: 'large',  # 50x50, guaranteed ratio
     secure_image_url: true
 
 
