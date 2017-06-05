0dab84ee6908a2d57a541136aebcbe8a11205270
Claudia Terpstra <claudia_terpstra@hotmail.com>
2017-06-05 17:20:20 +0100

    implemented lectures in show course

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,13 +1,16 @@
 class LecturesController < ApplicationController
 before_action :set_course, only: [:new, :create, :show, :destroy]
 before_action :set_lecture, only: [:show]
+skip_after_action :verify_authorized
 
   def new
      @lecture = Lecture.new
+     # authorize @lecture
   end
 
   def create
     @lecture = Lecture.new(lecture_params)
+    # authorize @lecture
     @lecture.course = @course
     if @lecture.save
       redirect_to course_path(@course)
@@ -19,6 +22,7 @@ before_action :set_lecture, only: [:show]
     end
 
     def destroy
+      # authorize @lecture
       @lecture.destroy
     end
   end
