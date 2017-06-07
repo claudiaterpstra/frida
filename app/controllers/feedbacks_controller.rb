66509289ec0fdaf84188897671388e7feeee2c12
Claudia Terpstra <claudia_terpstra@hotmail.com>
2017-06-07 17:01:54 +0100

    ajaxified feedback

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,19 +1,30 @@
 class FeedbacksController < ApplicationController
   skip_after_action :verify_authorized
 
+  def show
+    @artwork = Artwork.find(params[:artwork_id])
+  end
+
+
   def create
     @feedback = Feedback.new(feedback_params)
-    @feedback.artwork = Artwork.find(params[:artwork_id])
-    @feedback.student = @feedback.artwork.user
-    @feedback.teacher = @feedback.artwork.lecture.course.user
+    @artwork = Artwork.find(params[:artwork_id])
+    @feedback.artwork = @artwork
+    if current_user == @feedback.artwork.lecture.course.user
+      @feedback.teacher = current_user
+    elsif current_user == @feedback.artwork.user
+      @feedback.student = current_user
+    end
     if @feedback.save
       respond_to do |format|
         format.html { redirect_to studio_path }
         format.js
+      end
     else
       respond_to do |format|
         format.html { render 'artworks/show' }
         format.js
+      end
     end
   end
 
