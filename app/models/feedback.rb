2392230fa29e3fd043b5207f199544b6bc735869
Hwei Tan <chienhwei.tan@gmail.com>
2017-06-12 14:39:12 +0100

    Added feedback to navbar

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -3,4 +3,5 @@ class Feedback < ApplicationRecord
   belongs_to :student, :class_name => 'User'
   belongs_to :teacher, :class_name => 'User'
   has_many :notifications, dependent: :destroy
+  validates :message, presence: :true
 end
