8e61ab17127d8c0019cb1180bbdece983e757a09
Rebekah Cheng <work@rebekahcheng.com>
2017-06-16 12:51:36 +0100

    changed stripe button colorr

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,8 +7,7 @@ class OrdersController < ApplicationController
 
   def create
     @course = Course.find(params[:course_id])
-    order  = Order.create!(course_id: @course.id, amount: @course.price, state: 'pending')
-
+    order  = Order.create(course_id: @course.id, amount: @course.price, state: 'pending')
     redirect_to new_order_payment_path(order)
   end
 end
